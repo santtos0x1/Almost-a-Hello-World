d84c97fda0662c1fc604b1c181311117cfa894b0
Santtos <ruanrodrigues393@gmail.com>
2026-01-22 15:50:50 -0300

    Adding a button

--- main/main.c
+++ main/main.c
@@ -5,36 +5,60 @@
 
 #define STACK_SIZE 200
 
-gpio_config_t io_conf = {
-    .pin_bit_mask = (1ULL << 16), 
+gpio_config_t led_io_conf = {
+    .pin_bit_mask = (1ULL << 16),
     .mode = GPIO_MODE_OUTPUT,
     .pull_up_en = GPIO_PULLUP_DISABLE,
     .pull_down_en = GPIO_PULLDOWN_DISABLE,
     .intr_type = GPIO_INTR_DISABLE
 };
 
-void vTaskCode( void * pin )
+gpio_config_t btn_io_conf = {
+    .pin_bit_mask = (1ULL << 18),
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_ENABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .intr_type = GPIO_INTR_POSEDGE
+};
+
+typedef struct {
+    uint8_t led_pin;
+    uint8_t btn_pin;
+} gpio_pinout;
+
+void vTaskCode( void * pvParameters )
 {
+    gpio_pinout * pins = ( gpio_pinout * ) pvParameters;
     const TickType_t xDelay = 100;
 
-    gpio_num_t pin_cast = *(gpio_num_t *)pin;
     for( ;; )
     {
-        gpio_set_level( pin_cast, 1 );
+        if(gpio_get_level( ( gpio_num_t )pins->btn_pin ) == 0 )
+        {
+            gpio_set_level( ( gpio_num_t )pins->led_pin, 1 );
+        }
+        else
+        {
+            gpio_set_level( ( gpio_num_t )pins->led_pin, 0 );
+        }
         vTaskDelay( xDelay );
     }
 }
 
 void taskBuilder( void )
 {
-    static uint8_t pin_1 = 16;
+    static gpio_pinout pinout;
+    pinout.led_pin = 16;
+    pinout.btn_pin = 18;
+
     TaskHandle_t xHandle = NULL;
 
-    xTaskCreate( vTaskCode, "Led HIGH", STACK_SIZE, &pin_1, tskIDLE_PRIORITY, &xHandle );
+    xTaskCreate( vTaskCode, "Led HIGH", STACK_SIZE, &pinout, tskIDLE_PRIORITY, &xHandle );
 }
 
 void app_main( void )
 {
-    gpio_config(&io_conf);
+    gpio_config( &led_io_conf );
+    gpio_config( &btn_io_conf );
     taskBuilder();
 }
